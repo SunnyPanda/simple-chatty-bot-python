e617ff1c30c0a22b8de25bdc1889f1259028538a
Ekaterina Kozlova <ekaterina.kozlova@sbb.ch>
2021-03-26 19:19:18 +0300

    Stages 1 and 2

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,1 +1,7 @@
-# write your code here
+print('Hello! My name is Christina.')
+print('I was created in 2021.')
+print('Please, remind me your name.')
+
+user_name = input()
+
+print('What a great name you have, ' + user_name + '!')
